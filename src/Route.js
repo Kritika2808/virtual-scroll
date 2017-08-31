ba92d4259ccb9343bde40d218b0c5dd1209c6845
Kritika <kratikasaxena2808@gmail.com>
2017-09-01 00:50:25 +0530

    Revert "Implementing list"
    This reverts commit 4ec821d836e0ea2cfe7435ff37a2b46e999a2412.

--- src/Route.js
+++ src/Route.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import {Router, Route, hashHistory} from 'react-router';
 // Import custom components
 import App from './App';
-import List from './List';
+import Table from './Table';
 
 class Routes extends Component {
 
@@ -11,7 +11,7 @@ class Routes extends Component {
         return(
             <Router history={hashHistory}>
                 <Route path={process.env.PUBLIC_URL + '/'} component={App}/>
-                <Route path={process.env.PUBLIC_URL + '/list'} component={List}/>
+                <Route path={process.env.PUBLIC_URL + '/table'} component={Table}/>
             </Router>
         );
     };
--- src/Table.js
+++ src/Table.js
@@ -1,73 +1,9 @@
 import React, { Component } from 'react';
-import { VirtualScroll } from 'react-virtual-scrolling';
-import { times } from 'lodash';
 
-export default class Table extends Component {
-
-  constructor() {
-    super();
-    this.scrollList = this.scrollList.bind(this);
-  }
-
-  render() {
-    const data = times(40);
-    return (
-      <div ref="list" onScroll={this.scrollList}>
-        <VirtualScroll
-            {...this.props}
-            ref="virtualScroll"
-            rows={data}
-            scrollContainerHeight={100}
-            totalNumberOfRows={(data.length) || 0}
-            rowHeight={25}
-            rowRenderer={this.contentRenderer.bind(this)}
-        />
-      </div>
-    );
-  }
-
-
-  renderRows(fromRow, toRow, styles) {
-    const rows = times(40);
-    const generatedRows = [];
-    for (let i = fromRow; i < toRow; i++) {
-        if (rows[i]) {
-            generatedRows.push(<tr style={styles}>
-                <th scope="row">{rows[i]}</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>);
-        }
+class Table extends Component {
+    render(){
+        return (<h1>Table Page</h1>);
     }
-    console.log('generatedRows,', generatedRows);
-    return generatedRows;
-  }
-
-  scrollList(e) {
-    if (this.refs.virtualScroll) {
-      this.refs.virtualScroll.scrollHook(e.target);
-    }
-  }
-
-  contentRenderer(rowStyles, fromRow, toRow, parentStyles) {
-    console.log('fromrow, torow,', fromRow, toRow);// 1000px = 45rows * 25px
-    console.log('rowStyles,', rowStyles);
-    return (
-      <table className="table">
-        <thead>
-            <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
-            </tr>
-        </thead>
-        <tbody style={parentStyles}>
-            {this.renderRows(fromRow, toRow, rowStyles)}
-        </tbody>
-      </table>
-    );
-  }
 }
 
+export default Table;
