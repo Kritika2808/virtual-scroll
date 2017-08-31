9c392e144faa681e2bd927e6d56b57fcfa9d4fb1
Kritika <kratikasaxena2808@gmail.com>
2017-08-31 15:35:55 +0530

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Router, Route} from 'react-router';
-import './index.css';
-import Routes from './Route';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Routes/>, document.getElementById('root'));
-registerServiceWorker();
